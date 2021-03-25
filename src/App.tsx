5acef5ec0842d37e850ea669f04f20125c9479c2
bacebu4 <vasua14735@icloud.com>
2021-03-25 08:24:35 +0300

    layout: created main layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { FlexBox } from "react-layout-library";
+
 const App: React.FC = () => {
-  return <div>hey</div>;
+  return (
+    <>
+      <FlexBox jc="center" ai="center" h="100vh">
+        <div>hey</div>
+        <div>hey</div>
+      </FlexBox>
+    </>
+  );
 };
 
 export default App;
