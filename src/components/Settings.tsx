0780a0bea758863d09ba9abac19062a532157dde
bacebu4 <vasua14735@icloud.com>
2021-03-27 07:37:47 +0300

    layout: added styles to modal

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,5 +1,26 @@
 import styled from "styled-components";
+import Modal from "react-modal";
+import { useState } from "react";
+import { FlexBox } from "react-layout-library";
 import settingsButtonImage from "../assets/settingsButtonImage.png";
+import { CloseButton } from "./CloseButton";
+
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+    width: "30%",
+    maxHeight: "60%",
+    border: 0,
+    borderRadius: "20px",
+    paddingLeft: "44px",
+    paddingRight: "44px",
+  },
+};
 
 const SettingsButton = styled.img`
   position: absolute;
@@ -11,10 +32,48 @@ const SettingsButton = styled.img`
   cursor: pointer;
 `;
 
+const SettingsTitle = styled.h1`
+  font-size: 32px;
+  font-weight: 600;
+`;
+
+const SettingsUppercase = styled.h2`
+  color: #949494;
+  font-weight: 600;
+  text-transform: uppercase;
+`;
+
+const SettingsExplanation = styled.p`
+  color: #949494;
+  font-weight: 400;
+  line-height: 1.6rem;
+  max-width: 80%;
+`;
+
 export const Settings: React.FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <>
-      <SettingsButton src={settingsButtonImage} />
+      <SettingsButton
+        src={settingsButtonImage}
+        onClick={() => setIsOpen(true)}
+      />
+      <Modal
+        isOpen={isOpen}
+        onRequestClose={() => setIsOpen(false)}
+        style={customStyles}
+        contentLabel="Whatever"
+      >
+        <FlexBox jc="space-between" mt={16}>
+          <SettingsTitle>Settings</SettingsTitle>
+          <CloseButton onClick={() => setIsOpen(false)} />
+        </FlexBox>
+
+        <section>
+          <SettingsUppercase>Important some</SettingsUppercase>
+          <SettingsExplanation>Use it wisely here</SettingsExplanation>
+        </section>
+      </Modal>
     </>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import Modal from "react-modal";
 import App from "./App";
+
+Modal.setAppElement("#root");
+if (
+  Modal.defaultStyles &&
+  Modal.defaultStyles.overlay &&
+  Modal.defaultStyles.overlay.backgroundColor
+) {
+  Modal.defaultStyles.overlay.backgroundColor = "#0000005f";
+}
 
 ReactDOM.render(
   <React.StrictMode>
