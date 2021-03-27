fafcea74da2a819fbc2d6c56d163aa52c7c2e918
bacebu4 <vasua14735@icloud.com>
2021-03-27 07:09:08 +0300

    layout: showcasing new elems

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,21 @@ import { FlexBox } from "react-layout-library";
 import { LaterCard } from "./components/LaterCard";
 import { LinkElement } from "./components/LinkElement";
 import { Time } from "./components/Time";
+import { Weather } from "./components/Weather";
 
 const App: React.FC = () => {
   return (
     <>
       <FlexBox jc="center" ai="center" h="100vh" direction="column">
+        <Weather />
+
         <Time />
-        <LinkElement link={{ title: "GH Flow", url: "docs.github.com" }} />
+        <LinkElement
+          mt={32}
+          link={{ title: "GH Flow", url: "docs.github.com" }}
+        />
         <LaterCard
+          mt={32}
           link={{ title: "GH Flow", url: "docs.github.com", time: 3 }}
         />
       </FlexBox>
