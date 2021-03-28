520f6819becb5eb574003b4d1dc8e88b1926bf54
bacebu4 <vasua14735@icloud.com>
2021-03-28 07:15:34 +0300

    layout: done main layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { FlexBox } from "react-layout-library";
-import { LaterCard } from "./components/LaterCard";
+import { Laters } from "./components/Laters";
 import { Links } from "./components/Links";
 import { Settings } from "./components/Settings";
 import { Time } from "./components/Time";
@@ -12,12 +12,12 @@ const allLinks: Link[] = [
   { url: "https://vk.com/", title: "VK2" },
   { url: "https://vk.com/", title: "V3" },
   { url: "https://vk.com/", title: "VK4" },
-  { url: "https://vk.com/", title: "V" },
-  { url: "https://vk.com/", title: "VK" },
+  // { url: "https://vk.com/", title: "V" },
+  // { url: "https://vk.com/", title: "VK" },
 ];
 
 const App: React.FC = () => {
-  const [links, setLinks] = useState<Array<Link>>(allLinks);
+  const [links] = useState<Array<Link>>(allLinks);
   return (
     <>
       <Weather />
@@ -26,10 +26,7 @@ const App: React.FC = () => {
       <FlexBox jc="center" ai="center" h="100vh" direction="column">
         <Time />
         <Links links={links} />
-        <LaterCard
-          mt={32}
-          link={{ title: "GH Flow", url: "docs.github.com", time: 3 }}
-        />
+        <Laters mt={32} />
       </FlexBox>
     </>
   );
